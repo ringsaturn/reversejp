3f882608f15c477207139808afc98cc51c78c95c
Han Xiao <ringsaturn.me@gmail.com>
2025-10-18 17:30:37 +0900

    Upgrade preview to ts based map (#11)

--- reversejp-wasm/example/index.ts
+++
@@ -1,7 +0,0 @@
-import init, { initialize, find_properties } from "reversejp-wasm";
-
-await init();
-initialize();
-
-const results = find_properties(139.767, 35.6812);
-console.log(JSON.stringify(results, null, 2));
